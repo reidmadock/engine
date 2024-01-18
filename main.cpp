b812854f6344a6115d687824841fc277227b7498
Reid M <reidmadock@gmail.com>
2024-01-18 12:22:34 -0600

    Visual Studio is messing with my Git ugh

--- main.cpp
+++ main.cpp
@@ -1,5 +1,84 @@
+/*
+	To start off I am following along with Javidx9's "Code-It Yourself!" tutorial
+	https://www.youtube.com/watch?v=xW8skO7MFYw&list=PLrOv9FMX8xJE8NgepZR1etrsU63fDDGxO&index=2
+
+	Javidx9 himself is basing this version on the Wolfenstein 3D engine.
+
+	I'd like to make my own adjustments as I learn more and adjust
+
+	very introductory stuff here
+*/
 #include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int nScreenWidth = 120;
+int nScreenHeight = 40;
+
+float fPlayerX = 0.0f;
+float fPlayerY = 0.0f;
+float fPlayerA = 0.0f;
+
+int nMapHeight = 16;
+int nMapWidth = 16;
+
+float fFOV = 3.14159 / 4.0;
+
+float fDepth = 16.0f;
 
 int main() {
+	// Create Screen Buffer
+	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+	DWORD dwBytesWritten = 0;
+
+	wstring map;
+	map += L"################";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"################";
+
+
+	// Game Loop
+	while (1) {
+		for (int i = 0; i < length; i++)
+		{
+			// For each col, calculate the projected ray angle into the world
+			float fRayAngle = (fPlayerA - fFOV / 2.0f) + ((float)i / (float)nScreenWidth) * fFOV;
+
+			float fDistanceToWall = 0;
+			bool bHitWall;
+
+			float fEyeX = sinf(fRayAngle); 
+			float fEyeY = cosf(fRayAngle);
+
+			while (!bHitWall && fDistanceToWall < fDepth) {
+				fDistanceToWall += 0.1f;
+
+				int nTestX = (int)(fPlayerX + fEyeX * fDistanceToWall);
+				int nTestY = (int)(fPlayerY + fEyeY * fDistanceToWall);
+
+			}
+		}
+
+		screen[nScreenWidth * nScreenHeight - 1] = '\0';
+		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+
+	}
+
 	return 0;
 }
